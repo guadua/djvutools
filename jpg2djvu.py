a669a522f12268d47e5883607d418ec9c2d7d2f1
Hajime Tanaka <hajime.tanaka@gmail.com>
2021-08-11 08:09:54 +0900

    updated jpg2djvu.py

--- jpg2djvu.py
+++ jpg2djvu.py
@@ -17,9 +17,10 @@ def has_text(djvu):
     return ret.startswith(b'(page 0 0 0 0') == False
 
 def main():
-    cwd = os.getcwd().split('/')[-1] + '.djvu'
+    cwd = os.getcwd().split('/')[-1]
     # merge
     djvm_cmd = 'djvm -c %s.djvu ????.djvu' % cwd
+
     jpg_files = sorted([x for x in os.listdir('.') if x.endswith('.jpg')])
     df = pd.DataFrame(jpg_files)
     df[['pbm', 'djvu']] = df.apply(find_product, axis=1)
@@ -27,7 +28,7 @@ def main():
     cmds = []
     for index, row in df.iterrows():
         jpg, pbm, djvu = row
-    
+
         if not os.path.exists(pbm) or os.stat(pbm).st_size == 0:
             cmds.append('convert -density %s %s %s ' % (density, jpg, pbm))
         if not os.path.exists(djvu) or os.stat(pbm).st_size == 0:
